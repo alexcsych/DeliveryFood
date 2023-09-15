39e70f65186f05ec271b026be8469fa9bc4b56d2
alexcsych <alexsejsi4@gmail.com>
2023-09-15 17:55:47 +0300

    Add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Header.module.sass'
+
+function Header () {
+  return (
+    <div className={styles.Header}>
+      <div className={styles.Logo}>
+        <img src='/images/Logo.png' alt='Logo' className={styles.img} />
+        <div className={styles.div}>
+          <p className={styles.p}>Delivery</p>
+          <p className={styles.p}>Food</p>
+        </div>
+      </div>
+      <div className={styles.SearchContainer}>
+        <img src='/images/Home.png' alt='Home' />
+        <input
+          className={styles.SearchBar}
+          type='text'
+          placeholder='Адрес доставки'
+        />
+      </div>
+      <div className={styles.BtnsContainer}>
+        <button className={styles.BtnLogIn}>
+          <img src='/images/User.png' alt='User' />
+          <p>Войти</p>
+        </button>
+        <button className={styles.BtnCart}>
+          <img src='/images/Cart.png' alt='Cart' />
+          <p>Корзина</p>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
