33d016aa8b13663b81a2dba72c6f9d3db480779c
alexcsych <alexsejsi4@gmail.com>
2023-09-15 17:55:29 +0300

    Add pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Home.module.sass'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Banner from '../../components/Banner'
+import Menu from '../../components/Menu'
+
+function Home () {
+  return (
+    <div className={styles.Home}>
+      <Header></Header>
+      <Banner></Banner>
+      <Menu></Menu>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Home
