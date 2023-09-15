62e757b5ec79ec4d1d9e576bfa3375dfe3f47f00
alexcsych <alexsejsi4@gmail.com>
2023-09-15 17:56:11 +0300

    Add menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Menu.module.sass'
+import menuData from './Menu.json'
+import MenuItem from '../MenuItem'
+
+function Menu () {
+  return (
+    <>
+      <div className={styles.MenuHeader}>
+        <h2>Рестораны</h2>
+        <div className={styles.SearchContainer}>
+          <img src='/images/Search.png' alt='Search' />
+          <input
+            className={styles.SearchBar}
+            type='text'
+            placeholder='Поиск блюд и ресторанов'
+          />
+        </div>
+      </div>
+      <div className={styles.Menu}>
+        {menuData.map(menu => (
+          <MenuItem key={menu.id} menuName={menu.name} image={menu.image} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Menu
---
+++ src/components/MenuItem/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './MenuItem.module.sass'
+
+function MenuItem ({ menuName, image }) {
+  const imagePath = `/images/${image}.png`
+
+  return (
+    <div className={styles.MenuItem}>
+      <img src={imagePath} alt={image} className={styles.MenuImg} />
+      <div className={styles.MenuItemContent}>
+        <div className={styles.MenuName}>
+          <p className={styles.Name}>{menuName}</p>
+          <p className={styles.Time}>50 мин</p>
+        </div>
+        <div className={styles.MenuAbout}>
+          <div className={styles.MenuRating}>
+            <img src='/images/Star.png' alt='Star' className={styles.Star} />
+            <p className={styles.Rating}>4.5</p>
+          </div>
+          <div className={styles.MenuPrice}>
+            <p>От 900 грн</p>
+            <img
+              src='/images/Ellipse.png'
+              alt='Ellipse'
+              className={styles.Ellipse}
+            />
+            <p>Пицца</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MenuItem
