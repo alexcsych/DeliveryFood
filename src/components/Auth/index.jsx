d290ad71989a319f85e0b2143b551caf332e63da
alexcsych <alexsejsi4@gmail.com>
2023-11-03 13:18:05 +0200

    Add formik yup body-scroll-lock

--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -1,63 +1,79 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import styles from './Auth.module.sass'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import { connect } from 'react-redux'
 import { hideAuth } from '../../store/slices/authSlice'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 
 function Auth ({ hide, authData }) {
-  const [formData, setFormData] = useState({
+  const initialValues = {
     username: '',
     password: ''
+  }
+
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .trim()
+      .required('Имя пользователя обязательно для заполнения')
+      .min(3, 'Имя пользователя должно содержать как минимум 3 символа'),
+    password: Yup.string()
+      .trim()
+      .required('Пароль обязателен для заполнения')
+      .min(6, 'Пароль должен содержать как минимум 6 символов')
   })
 
-  const handleInputChange = e => {
-    const { name, value } = e.target
-    setFormData({
-      ...formData,
-      [name]: value
-    })
-  }
-
-  const handleFormSubmit = e => {
-    e.preventDefault()
-    localStorage.setItem('username', formData.username)
-    localStorage.setItem('password', formData.password)
-    setFormData({ username: '', password: '' })
+  const handleSubmit = (values, { resetForm }) => {
+    localStorage.setItem('username', values.username)
+    localStorage.setItem('password', values.password)
+    resetForm()
     hide()
   }
+
+  useEffect(() => {
+    if (authData.isVisible) {
+      disableBodyScroll(document.body)
+    } else {
+      enableBodyScroll(document.body)
+    }
+  }, [authData.isVisible])
 
   return (
     authData.isVisible && (
       <>
-        <div
-          onClick={() => {
-            hide()
-          }}
-          className={styles.overlay}
-        ></div>
+        <div onClick={hide} className={styles.overlay}></div>
         <div className={styles.modal}>
-          <form onSubmit={handleFormSubmit}>
-            <div className={styles.field}>
-              <label htmlFor='username'>Имя:</label>
-              <input
-                type='text'
-                id='username'
-                name='username'
-                value={formData.username}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className={styles.field}>
-              <label htmlFor='password'>Пароль:</label>
-              <input
-                type='password'
-                id='password'
-                name='password'
-                value={formData.password}
-                onChange={handleInputChange}
-              />
-            </div>
-            <button type='submit'>Сохранить</button>
-          </form>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            <Form>
+              <div className={styles.fieldBox}>
+                <div className={styles.field}>
+                  <label htmlFor='username'>Имя:</label>
+                  <Field type='text' id='username' name='username' />
+                </div>
+                <ErrorMessage
+                  name='username'
+                  component='div'
+                  className={styles.errorMessage}
+                />
+              </div>
+              <div className={styles.fieldBox}>
+                <div className={styles.field}>
+                  <label htmlFor='password'>Пароль:</label>
+                  <Field type='password' id='password' name='password' />
+                </div>
+                <ErrorMessage
+                  name='password'
+                  component='div'
+                  className={styles.errorMessage}
+                />
+              </div>
+              <button type='submit'>Сохранить</button>
+            </Form>
+          </Formik>
         </div>
       </>
     )
